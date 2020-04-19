dd33bbf57ed20790c20bc7dbd7bc1c9b2e69769f
alifgiant <muh.alifgiant@gmail.com>
2020-04-19 22:54:22 +0800

    setup data

--- asset.js
+++ asset.js
@@ -4,21 +4,35 @@ const fs = require('fs');
 var getDirectories = function(src, callback) {
     glob(src + '/**/*', callback);
 };
+
 getDirectories('public/assets/images/portfolio', function(err, res) {
-    var asset = {};
+    var asset = {
+        'types': [],
+        'images': [],
+    };
     for (i in res) {
         const name = res[i].split("/");;
-        if (!(name[4] in asset)) {
-            asset[name[4]] = [];
-        }
-        if (name[5] != null) {
-            asset[name[4]].push(name[5]);
+        if (name[5] == null) {
+            asset.types.push(name[4]);
+        } else {
+            asset.images.push({
+                'type': name[4],
+                'name': name[5]
+            });
         }
     }
 
-    var json = 'asset = ' + JSON.stringify(asset) + ';';
-    fs.writeFile('public/assets/js/picasset.js', json, function(err) {
-        if (err) throw err;
-        console.log('asset recap complete');
+    fs.readFile('config.json', 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            obj = JSON.parse(data); //now it an object
+            obj['asset'] = asset;
+            json = JSON.stringify(obj); //convert it back to json
+            fs.writeFile('config.json', json, 'utf8', function(err) {
+                if (err) throw err;
+                console.log('asset recap complete');
+            }); // write it back 
+        }
     });
 });
